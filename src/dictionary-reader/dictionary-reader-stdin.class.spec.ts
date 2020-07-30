d032de18148fc1e8ec70abed62b920aa3da676f8
Anthony Bonta <zthun.coding@gmail.com>
2020-07-30 10:33:21 -0500

    Added unit tests for the stdin dictionary reader.

---
+++ src/dictionary-reader/dictionary-reader-stdin.class.spec.ts
@@ -0,0 +1,81 @@
+import { prompt } from 'inquirer';
+import { ZDictionaryReaderStdIn } from './dictionary-reader-stdin.class';
+import { IZValueReaderFactory } from '../value-reader/value-reader-factory.interface';
+import { noop } from 'lodash';
+import { ZValueReaderFactory } from '../value-reader/value-reader-factory.class';
+import { IZValueReader } from '../value-reader/value-reader.interface';
+import { ZValueReaderKeep } from '../value-reader/value-reader-keep.class';
+import { ZValueStrategy } from '../value-reader/value-strategy.enum';
+import { ZValueReaderEmpty } from '../value-reader/value-reader-empty.class';
+
+jest.mock('inquirer');
+
+describe('ZDictionaryReaderStdin', () => {
+  let reader: IZValueReader;
+  let def: IZValueReader;
+  let factory: IZValueReaderFactory;
+
+  function createTestTarget() {
+    return new ZDictionaryReaderStdIn(console, factory);
+  }
+
+  beforeEach(() => {
+    def = new ZValueReaderEmpty();
+    reader = new ZValueReaderKeep();
+    factory = new ZValueReaderFactory();
+
+    jest
+      .spyOn(reader, 'read')
+      .mockClear()
+      .mockImplementation((key) => Promise.resolve(key));
+
+    jest.spyOn(def, 'read');
+
+    jest.spyOn(factory, 'create').mockClear().mockReturnValue(reader);
+    jest.spyOn(factory, 'default').mockClear().mockReturnValue(def);
+
+    jest.spyOn(console, 'log').mockClear().mockImplementation(noop);
+    jest.spyOn(console, 'error').mockClear().mockImplementation(noop);
+
+    ((prompt as unknown) as jest.SpyInstance).mockResolvedValue({ strategy: ZValueStrategy.LeaveAsIs });
+  });
+
+  it('should prompt the user for the options for each variable.', async () => {
+    // Arrange
+    const target = createTestTarget();
+    // Act
+    await target.read(['a', 'b', 'c']);
+    // Assert
+    expect(prompt).toHaveBeenCalledTimes(3);
+  });
+
+  it('should return a dictionary with all the values to the given keys.', async () => {
+    // Arrange
+    const target = createTestTarget();
+    // Act
+    const actual = await target.read(['a', 'b', 'c']);
+    // Assert
+    expect(`${actual.a}${actual.b}${actual.c}`).toEqual('abc');
+  });
+
+  it('should return the default reader value if there is an error reading the value at any point.', async () => {
+    // Arrange
+    const target = createTestTarget();
+    ((reader.read as unknown) as jest.SpyInstance).mockRejectedValue('failed');
+    ((def.read as unknown) as jest.SpyInstance).mockResolvedValue('d');
+    // Act
+    const actual = await target.read(['a', 'b', 'c']);
+    // Assert
+    expect(`${actual.a}${actual.b}${actual.c}`).toEqual('ddd');
+  });
+
+  it('should notify the user that the value failed to read.', async () => {
+    // Arrange
+    const target = createTestTarget();
+    ((reader.read as unknown) as jest.SpyInstance).mockRejectedValue('failed');
+    // Act
+    await target.read(['a']);
+    // Assert
+    expect(console.error).toHaveBeenCalledTimes(1);
+  });
+});
